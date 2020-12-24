45654e5cd034e9813dfa2258c0fe4abef02bda4a
Siyamak - macOS <>
2020-12-24 11:25:20 -0800

    sync torch state on start (iOS)

--- ios/IPDFCameraViewController.m
+++ ios/IPDFCameraViewController.m
@@ -150,6 +150,11 @@
         }
     }
 
+    dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * 0.5);
+    dispatch_after(delay, dispatch_get_main_queue(), ^(void){
+        [self setEnableTorch:self.enableTorch];
+    });
+
     [session commitConfiguration];
 }
 
